e0cf30dc0cdf163cb6387f2a8ac11469d1fb523e
Om Patel <ompatel@Oms-MacBook-Pro.local>
2023-08-22 16:44:23 -0400

    Add list dropdown animation, and increase usability in mobile mode

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 $(".list-icon").on("click", function(){
-    $(".inner-mobile-list").toggle();
+    $(".list-icon").css("display", "none");
+    $(".x-icon").css("display", "unset");
+
+    $(".x-icon").on("click", function(){
+        $(".x-icon").css("display", "none");
+        $(".list-icon").css("display", "unset")
+        if ($(".inner-mobile-list").is(":visible")){
+            $(".inner-mobile-list").css("display", "none");
+        }
+    });
+
+
+    $(".inner-mobile-list").toggle(function(){
+        $(".inner-mobile-list li").each(function(i) {
+            $(this).delay(100 * i).fadeIn(500);
+        });
+    });
+
+
 });
+ 
