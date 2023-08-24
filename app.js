8fc8fefa5fd04c319344e5f7ecb5ef82c84ba6a5
Om Patel <ompatel@Oms-MacBook-Pro.local>
2023-08-24 01:55:13 -0400

    Create cart page, add header

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import ejs from "ejs";
 
 const port = 3000; 
 const app = express(); 
 app.use(express.static("public"));
+
 
 app.listen(port, function(){
     console.log("Server started on port 3000.");
@@ -11,3 +13,7 @@ app.listen(port, function(){
 app.get("/", (req, res)=>{
     res.render("index.html");
 });
+
+app.get("/cart", (req, res)=>{
+    res.render("cart.ejs");
+})
